bc001714ded8ec499151d41ce32d613e6ef382f5
Wong, Joshua (k21090811) <joshua.1.wong@kcl.ac.uk>
2022-10-01 13:46:28 +0100

    Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    #raise valueerror if 0 or <0
+    if number_of_primes < 1:
+        raise ValueError("Input value can't be less than 1")
+
     list = []
+    num_counter = 2 #1 isn't prime
+
+    while len(list) != number_of_primes:
+        for i in range(2, num_counter):
+            if num_counter % i == 0:
+                break
+        else:
+            list.append(num_counter)
+        num_counter += 1
+
+         
     return list
